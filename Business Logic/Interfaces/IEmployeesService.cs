14b0313e8e1a444edcd053a2a705416542f6cd12
Javier lopez <javierlopezok@outlook.com>
2020-11-02 18:01:35 -0300

    Add project files.

---
+++ Business Logic/Interfaces/IEmployeesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Business_Logic
+{
+    public interface IEmployeeService
+    {
+
+    }
+}
---
+++ Entities/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Entities
+{
+
+    public class HourlyEmployee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RoleId { get; set; }
+
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+
+        public int HourlySalary { get; set; }
+    }
+
+    public class MonthlyEmployee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RoleId { get; set; }
+
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+
+        public int MonthlySalary { get; set; }
+    }
+
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+
+
+}
